72614a7d9a8bfff37338b6837763da200a8018e4
breakcup <haoyuan_zhuhai@outlook.com>
2017-08-20 11:48:50 +0800

    修改

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 var http = require('http');
 var router = require('./router.js')
 
-var server = http.createServer();
+var server = http.createServer((req,res)=>{
+	recive(req,res);
+}).listen(80,()=>{
+	console.log("开始监听");
+});
 
-server.on('require',function(req,res){
+var recive = function(req,res){
 	console.log("接收到请求");
 	req.setEncoding('utf-8');
 	router.route("http://39.108.117.154:80"+req.url);
 	response.writeHead(200,{"Content-type":"text/plain"});
 	response.write("aa");
 	response.end();
-});
+};
 
-server.listen(80,function(){
-	console.log("开始监听");
-});
+
 
 server.on('error', (e) => {
     if(e.code === 'EADDRINUSE') {
